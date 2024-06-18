25b2a625be05bf98c743e31b42ffbe92f29573e0
kbaraban <barabanov.clv@gmail.com>
2024-06-18 15:50:16 +0600

    update server part

--- main.go
+++ main.go
@@ -6,12 +6,36 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Yolo, Swagger!"))
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	w.Write([]byte("Hi, it`s Snippetbox"))
+}
+
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Showing a note..."))
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		//w.Header().Add("Rofler", "keker")
+		//w.Header()["Date"] = nil
+
+		http.Error(w, "Method not allowed", 405)
+
+		return
+	}
+	w.Write([]byte("Create new note..."))
 }
 
 func main() {
-	mux := http.NewServeMux()
+	mux := http.NewServeMux() // регистрация маршрутов
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
+
 	log.Println("Listening on http://localhost:4000...")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
